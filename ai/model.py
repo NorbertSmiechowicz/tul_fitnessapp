0a1044e0d1bbc6abfeb0892704c2cd1b393b60b9
brzozio <michal.brzozowski@interia.eu>
2024-06-15 14:03:32 +0200

    added model

---
+++ ai/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class RNN(nn.Module):
+    def __init__(self):
+
+        super(RNN, self).__init__()
+
+
+    def forward(self, out):
+        pass
+
+        return out
