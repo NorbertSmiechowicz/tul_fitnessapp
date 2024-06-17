dcb35773213d2db539395f34f75d51cbba93ffa1
brzozio <michal.brzozowski@interia.eu>
2024-06-17 19:37:14 +0200

    added path to data folder

--- model_ai/code/main.py
+++ model_ai/code/main.py
@@ -1,26 +1,30 @@
 from model import RNN
 import torch
 import os
-
 #Global path settings
-repo_name = "tul_fitnessapp"
+repo_name   = "tul_fitnessapp"
 path_script = os.path.dirname(os.path.realpath(__file__))
-path_dir = os.path.dirname(os.path.realpath(path_script))
-index = path_script.find(repo_name)
+path_dir    = os.path.dirname(os.path.realpath(path_script))
+index       = path_script.find(repo_name)
 path_models = path_dir + "\\models\\"
+path_data   = path_dir + "\\data\\"
 
 
 
 def save_model(model, name):
     torch.save(model, path_models + f'{name}.pth')
+    #LOGOWANIE 
 
 def load_model(name):
+    #LOGOWANIE 
     return torch.load(path_models + f'{name}.pth')
+
 
 
 def main():
     print(f"Path to models: {path_models}")
     print(f"Path to script: {path_script}")
+    print(f"Path to data: {path_data}")
     print(f"Path to AI_MODELS directory: {path_dir}")
     print(f"Torch version: {torch.__version__}")
 
