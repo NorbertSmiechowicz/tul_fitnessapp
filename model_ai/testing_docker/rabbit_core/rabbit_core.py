9a047fa797638948de15413fa864149abd4dd594
brzozio <michal.brzozowski@interia.eu>
2024-06-18 14:36:40 +0200

    backed to working version

--- model_ai/testing_docker/rabbit_core/rabbit_core.py
+++ model_ai/testing_docker/rabbit_core/rabbit_core.py
@@ -1,6 +1,15 @@
 from rabbitmq import RabbitMQ_comms
+from pymongo import MongoClient
 
 rabbit2 = RabbitMQ_comms()
+client = MongoClient('mongo', 27017)
+db = client.mydatabase
+collection = db.mycollection
+records = collection.find()
+
+for i, record in enumerate(records):
+    print(record)
+    rabbit2.send_message(f"Record [{i}] : {record}")
 
 while(True):
-    rabbit2.send_message("Hello world")
+    rabbit2.send_message("Test only str")
