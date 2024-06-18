365c01817835a38bda469a7b59e5f7d9f7c0e63f
adrianek2 <adrianfel218@gmail.com>
2024-06-18 19:58:01 +0200

    change communication rabit core to rabit ai in rabit core

--- model_ai/testing_docker/rabbit_core/rabbit_core.py
+++ model_ai/testing_docker/rabbit_core/rabbit_core.py
@@ -1,3 +1,4 @@
+"""
 from rabbitmq import RabbitMQ_comms
 from pymongo import MongoClient
 
@@ -19,6 +20,9 @@ rabbit2 = RabbitMQ_comms()
 while(1):
     for record in msg_list:
         rabbit2.send_message(record)
+        
+
+        
 
 # client = MongoClient("mongodb://localhost:27017/")
 # db = client["mydatabase"]
@@ -32,15 +36,51 @@ while(1):
 #     print(record['name'])
 #     print(type(record))
 
+"""
 
 import logging
 import logstash
+import sys
+from rabbitmq import RabbitMQ_comms
+from pymongo import MongoClient
 
-host = 'logstash'
+# Konfiguracja logowania
+logstash_host = 'logstash'
+logstash_port = 5044
 
 logger = logging.getLogger('rabbit_core')
 logger.setLevel(logging.INFO)
-logger.addHandler(logstash.TCPLogstashHandler(host, 5044, version=1))
+logstash_handler = logstash.TCPLogstashHandler(logstash_host, logstash_port, version=1)
+logger.addHandler(logstash_handler)
 
-# Example log
-logger.info('Rabbit Core Service started')
+# Możliwość logowania również do konsoli
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
+
+# Połączenie z MongoDB
+client = MongoClient('mongodb')
+db = client["mydatabase"]
+collection = db["mycollection"]
+
+# Wyszukiwanie w kolekcji MongoDB
+myquery = { "name": { "$regex": "^." } }
+mydoc = collection.find(myquery)
+
+msg_list = []
+for record in mydoc:
+    msg_list.append(f"imie : {record['name']}")
+    # Logowanie każdego rekordu
+    logger.info(f"Fetched record from MongoDB: {record}")
+
+# Inicjalizacja RabbitMQ
+rabbit2 = RabbitMQ_comms()
+
+# Pętla wysyłająca wiadomości
+while True:
+    for record in msg_list:
+        rabbit2.send_message(record)
+        logger.info(f"Sent message to RabbitMQ: {record}")
+
+
+
