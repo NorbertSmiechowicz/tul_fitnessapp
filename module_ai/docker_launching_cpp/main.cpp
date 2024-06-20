8e837a798b28bdf0a30567f2431ab74076c25481
NorbertSmiechowicz <norbert.smiechowicz@gmail.com>
2024-06-20 18:56:51 +0200

    successfuly passing a json file path to cpp executable and reading from it

--- module_ai/docker_launching_cpp/main.cpp
+++ module_ai/docker_launching_cpp/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
 
-    std::cout << "Have " << argc << " arguments:\n";
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << "\n";
+    std::ifstream input_file(argv[1]);
+
+    if (!input_file.is_open()) {
+        std::cerr << "Failed to open file: " << argv[1];
+        return -1;
     }
     
+    nlohmann::json json_data;
+    input_file >> json_data;
+    
+    input_file.close();
+
+    std::cout << "Parsed JSON data: " << json_data.dump(4) << "\n";
 }
--- module_ai/docker_launching_cpp/wonsz.py
+++ module_ai/docker_launching_cpp/wonsz.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
-path = os.path.dirname(os.path.realpath(__file__)) + "/bin/main"
+path = os.path.dirname(os.path.realpath(__file__))
+path_exe = path + "/bin/main"
+path_json = path + "\\test.json"
 
-subprocess.run([path, "jakub wlazlo 235678"])
+subprocess.run([path_exe, path_json])
