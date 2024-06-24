c258bce0a6652292555dc7750fc33ef1e1bfeb16
1anKoz <jankozinski@wp.pl>
2024-06-25 01:32:46 +0200

    Create exercise create page

--- module_core/code/web/src/main/java/com/FitFoodApp/web/controller/ExerciseController.java
+++ module_core/code/web/src/main/java/com/FitFoodApp/web/controller/ExerciseController.java
@@ -1,11 +1,14 @@
 package com.FitFoodApp.web.controller;
 
+import com.FitFoodApp.web.dto.ExerciseDto;
 import com.FitFoodApp.web.models.Exercise;
 import com.FitFoodApp.web.service.ExerciseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ExerciseController {
@@ -21,5 +24,13 @@ public class ExerciseController {
         model.addAttribute("userId", userId);
         model.addAttribute("exercise", exercise);
         return "exercises-create";
+    }
+
+    @PostMapping("/exercises/{userId}")
+    public String createExercise(@PathVariable("userId") int userId,
+                                 @ModelAttribute("exercise") ExerciseDto exerciseDto,
+                                 Model model) {
+        exerciseService.createExercise(userId, exerciseDto);
+        return "redirect:/users/" + userId;
     }
 }
